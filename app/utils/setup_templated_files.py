68985147c3802d47683276399a59cfd22345b715
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-03 18:17:42 +0100

    added args to setup_templates and moved into prod detection logic in main.py

--- app/utils/setup_templated_files.py
+++ app/utils/setup_templated_files.py
@@ -1,23 +1,20 @@
 import os
-from app.utils.load_settings_from_files import USER_SETTINGS_PATH
 
 import logging
 logger = logging.getLogger(__name__)
 
-TEMPLATES_DIR = "app/templates/"
-
-def setup_templates():
+def setup_templates(user_setting_path: str, templates_dir: str) -> None:
     def setup_user_settings_yml():
-        if not os.path.exists(USER_SETTINGS_PATH):
-            template_settings_path = os.path.join(TEMPLATES_DIR, "user-settings.yml")
+        if not os.path.exists(user_setting_path):
+            template_settings_path = os.path.join(templates_dir, "user-settings.yml")
             if not os.path.exists(template_settings_path):
                 logger.error(f"Template file {template_settings_path} does not exist.")
                 raise FileNotFoundError(f"Template file {template_settings_path} does not exist.")
             
-            logger.info(f"user-settings.yml does not exist. Copying template settings from {template_settings_path} to {USER_SETTINGS_PATH}")
+            logger.info(f"user-settings.yml does not exist. Copying template settings from {template_settings_path} to {user_setting_path}")
             with open(template_settings_path, 'r') as template_file:
                 template_content = template_file.read()
-            with open(USER_SETTINGS_PATH, 'w') as user_settings_file:
+            with open(user_setting_path, 'w') as user_settings_file:
                 user_settings_file.write(template_content)
     
     setup_user_settings_yml()
--- main.py
+++ main.py
@@ -8,14 +8,19 @@ from app.db.database import init_db, seed_settings
 from app.config import check_for_db_reset, APP_CFG
 from app.utils.setup_templated_files import setup_templates
 
-setup_templates()
+TEMPLATES_DIR = "app/templates/"
+USER_SETTINGS_PATH = "app/user/user-settings.yml"
+
 check_for_db_reset()
 
 if APP_CFG['MODE'] == "prod":
+    setup_templates(TEMPLATES_DIR, USER_SETTINGS_PATH)
+    logger.info(f"Templates setup completed. User settings path: {USER_SETTINGS_PATH}")
     init_db()
     logger.info("Database initialized.")
     seed_settings()
     logger.info("Database seeded with settings.")
+    
 
 app = FastAPI(
     title="Finance Tracker API",
