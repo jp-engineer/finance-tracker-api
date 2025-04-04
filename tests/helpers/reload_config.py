904d5b1ce640654bc2871845f80a07b1f2162d23
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-04 12:35:44 +0100

    added reload_config helper module

--- tests/helpers/reload_config.py
+++ tests/helpers/reload_config.py
@@ -1,13 +1,13 @@
-# import os
-# import sys
-# import importlib
+import os
+import sys
+import importlib
 
-# CONFIG_PATH = "app.config"
+CONFIG_PATH = "app.config"
 
-# def reload_config_module_with_mode(mode_value):
-#     os.environ["MODE"] = mode_value
-#     if CONFIG_PATH in sys.modules:
-#         del sys.modules[CONFIG_PATH]
+def reload_config_module_with_mode(mode_value):
+    os.environ["MODE"] = mode_value
+    if CONFIG_PATH in sys.modules:
+        del sys.modules[CONFIG_PATH]
         
-#     cfg_mod = importlib.import_module(CONFIG_PATH)
-#     return cfg_mod
+    cfg_mod = importlib.import_module(CONFIG_PATH)
+    return cfg_mod
