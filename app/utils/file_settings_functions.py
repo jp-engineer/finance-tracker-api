ea969e5eb0a41c4b1cae9995d7012edb42d738f9
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-04 06:03:07 +0100

    replaced load merged settings

--- app/utils/file_settings_functions.py
+++ app/utils/file_settings_functions.py
@@ -1,8 +1,8 @@
+from datetime import date
 import yaml
 from pydantic import ValidationError
 from app.schemas.setting import SettingBase
 from app.schemas.enums import SettingCategoryEnum
-from app.utils.setup_templated_files import setup_templates
 
 import logging
 logger = logging.getLogger(__name__)
@@ -18,46 +18,12 @@ def validate_setting(category: str, key: str, value: str) -> bool:
     except ValidationError as e:
         logger.warning(f"Invalid setting [{category}.{key}] = {value}: {e}")
         return False
-    
-        
-def load_merged_settings(user_settings_path: str ="app/user/user-settings.yml",
-                         default_settings_path: str ="app/defaults/default-settings.yml"):
-    def read_yaml(path):
-        try:
-            data = yaml.safe_load(open(path,  "r", encoding='utf-8'))
-            return data
-        except FileNotFoundError:
-            logger.error(f"File {path} not found. Returning empty dictionary.")
-
-    user = read_yaml(user_settings_path)
-    default = read_yaml(default_settings_path)
-    merged = {}
-
-    for category, keys in default.items():
-        merged[category] = {}
-        for key, default_val in keys.items():
-            user_val = user.get(category, {}).get(key, None)
-
-            if user_val is not None:
-                try:
-                    SettingBase(
-                        category=SettingCategoryEnum(category),
-                        key=key,
-                        value=str(user_val)
-                    )
-                    merged[category][key] = user_val
-                except ValidationError as e:
-                    logger.warning(f"Invalid setting [{category}.{key}] = {user_val}: {e}")
-                    merged[category][key] = default_val
-            else:
-                merged[category][key] = default_val
-
-    logging.debug(f"Merged and validated settings: {merged}")
-    return merged
 
 def update_all_user_settings(user_settings_path: str ="app/user/user-settings.yml", settings: dict = None) -> dict:
     for category, keys in settings.items():
         for key, value in keys.items():
+            if value is None and key == "start_date":
+                value = date.today().strftime("%Y-%m-%d")
             if not validate_setting(category, key, value):
                 raise ValueError(f"Invalid setting: {category}.{key} = {value}")
 
