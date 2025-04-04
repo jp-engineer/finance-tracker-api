90cb9e7b1e52d77d75de0a899c8e5ff96a3501e2
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-04 22:24:22 +0100

    added validation of test post e2e testing

--- tests/app/api/v1/routes/post/test_post_e2e_testing.py
+++ tests/app/api/v1/routes/post/test_post_e2e_testing.py
@@ -4,20 +4,27 @@ pytestmark = [
     pytest.mark.api_e2e_testing
 ]
 
-from tests.helpers.read_test_data import load_test_data_file
+from app.db.utils.get_db_functions import get_all_settings_from_db
+from tests.helpers.read_test_data import check_test_db_path_exists, load_test_data_file
 
 ROUTE = "/e2e-testing"
 
 class TestAPIIndex:
     def test_post_init_blank_test_db(self, e2e_client, api_prefix):
         response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-init-db")
+        db_exists = check_test_db_path_exists()
+
         assert response.status_code == 200
+        assert db_exists is True
 
     def test_post_seed_db_settings_data(self, e2e_client, api_prefix):
         post_settings = load_test_data_file("test_post_e2e_testing_settings_data.json")
         response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-seed-settings", json=post_settings)
+        
+        seeded_settings = get_all_settings_from_db()
         assert response.status_code == 200
-    
+        assert seeded_settings == post_settings
+
     def test_post_seed_db_all_data(self, e2e_client, api_prefix):
         post_settings = load_test_data_file("test_post_e2e_testing_all_data.json")
         response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-seed-data", json=post_settings)        
