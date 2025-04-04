21d518a65ca6c6bf3740958a21b6e0424eca605a
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-04 08:19:07 +0100

    added empty unit test placeholders

--- app/api/v1/routes/get/settings.py
+++ app/api/v1/routes/get/settings.py
@@ -1,23 +1,23 @@
-from fastapi import APIRouter, HTTPException
-from app.db.utils.get_db_functions import get_all_settings_from_db, get_setting_from_db
-from app.schemas.setting import SettingRead
+# from fastapi import APIRouter, HTTPException
+# from app.db.utils.get_db_functions import get_all_settings_from_db, get_setting_from_db
+# from app.schemas.setting import SettingRead
 
-import logging
-logger = logging.getLogger(__name__)
+# import logging
+# logger = logging.getLogger(__name__)
 
-router = APIRouter(prefix="/settings")
+# router = APIRouter(prefix="/settings")
 
-# --- Settings Routes ---
-@router.get("/get-all-settings")
-def get_all_settings():
-    logger.info("GET /get-all-settings")
-    settings_dicts = get_all_settings_from_db()
-    return settings_dicts
+# # --- Settings Routes ---
+# @router.get("/get-all-settings")
+# def get_all_settings():
+#     logger.info("GET /get-all-settings")
+#     settings_dicts = get_all_settings_from_db()
+#     return settings_dicts
 
-@router.get("/get-setting-by-category-and-key/{category}/{key}", response_model=SettingRead)
-def get_setting_by_category_and_key(category: str, key: str):
-    logger.info(f"GET /get-setting-by-category-and-key/{category}/{key}")
-    setting_dict = get_setting_from_db(category, key)
-    if not setting_dict:
-        raise HTTPException(status_code=404, detail="Setting not found")
-    return setting_dict
+# @router.get("/get-setting-by-category-and-key/{category}/{key}", response_model=SettingRead)
+# def get_setting_by_category_and_key(category: str, key: str):
+#     logger.info(f"GET /get-setting-by-category-and-key/{category}/{key}")
+#     setting_dict = get_setting_from_db(category, key)
+#     if not setting_dict:
+#         raise HTTPException(status_code=404, detail="Setting not found")
+#     return setting_dict
--- app/api/v1/routes/put/settings.py
+++ app/api/v1/routes/put/settings.py
@@ -1,17 +1,17 @@
-from fastapi import APIRouter
-from app.db.utils.put_db_functions import put_all_settings_to_db
-from app.utils.file_settings_functions import update_all_user_settings
+# from fastapi import APIRouter
+# from app.db.utils.put_db_functions import put_all_settings_to_db
+# from app.utils.file_settings_functions import update_all_user_settings
 
-import logging
-logger = logging.getLogger(__name__)
+# import logging
+# logger = logging.getLogger(__name__)
 
-router = APIRouter(prefix="/settings")
+# router = APIRouter(prefix="/settings")
 
-# --- Settings Routes ---
-@router.put("/put-all-settings")
-def put_all_settings(input_settings: dict):
-    logger.info("PUT /put-all-settings")
-    put_all_settings_to_db(settings=input_settings)
-    update_all_user_settings(settings=input_settings)
+# # --- Settings Routes ---
+# @router.put("/put-all-settings")
+# def put_all_settings(input_settings: dict):
+#     logger.info("PUT /put-all-settings")
+#     put_all_settings_to_db(settings=input_settings)
+#     update_all_user_settings(settings=input_settings)
 
-    return {"message": "Settings updated successfully"}
+#     return {"message": "Settings updated successfully"}
