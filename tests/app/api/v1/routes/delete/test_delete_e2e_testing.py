9df9e1612e65e7021e494101b6a2371713195b64
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-04 20:39:15 +0100

    added tdd api tests for e2e_testing postdb

---
+++ tests/app/api/v1/routes/delete/test_delete_e2e_testing.py
@@ -0,0 +1,13 @@
+import pytest
+pytestmark = [
+    pytest.mark.api,
+    pytest.mark.api_e2e_testing
+]
+
+ROUTE = "/e2e-testing"
+
+class TestAPIIndex:
+    def test_get_e2e_mode_check(self, e2e_client, api_prefix):
+        response = e2e_client.get(f"{api_prefix}{ROUTE}/db/delete-db")
+        
+        assert response.status_code == 200
--- tests/app/api/v1/routes/post/test_post_e2e_testing.py
+++ tests/app/api/v1/routes/post/test_post_e2e_testing.py
@@ -4,20 +4,21 @@ pytestmark = [
     pytest.mark.api_e2e_testing
 ]
 
+from tests.helpers.read_test_data import load_test_data_file
+
 ROUTE = "/e2e-testing"
 
 class TestAPIIndex:
     def test_post_init_blank_test_db(self, e2e_client, api_prefix):
         response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-init-db")
-        
         assert response.status_code == 200
 
     def test_post_seed_db_settings_data(self, e2e_client, api_prefix):
-        response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-seed-settings")
-        
+        post_settings = load_test_data_file("test_post_e2e_testing_settings_data.json")
+        response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-seed-settings", json=post_settings)
         assert response.status_code == 200
     
     def test_post_seed_db_all_data(self, e2e_client, api_prefix):
-        response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-seed-data")
-        
+        post_settings = load_test_data_file("test_post_e2e_testing_all_data.json")
+        response = e2e_client.post(f"{api_prefix}{ROUTE}/db/post-seed-data", json=post_settings)        
         assert response.status_code == 200
