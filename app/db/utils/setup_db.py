16d6ab46b905bceef5930b2cadad3acdb292be3b
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-09 20:23:31 +0100

    added seeding of db

--- app/db/utils/setup_db.py
+++ app/db/utils/setup_db.py
@@ -1,13 +1,11 @@
 import os
-from datetime import date
+from datetime import date, datetime
 from sqlalchemy.orm import Session
 from pydantic import ValidationError
+import finance_tracker_shared.schemas as schemas
 from app.config import APP_CFG
-from app.db.models import Base
-from app.db.models import SettingGeneral, SettingDeveloper, SettingView
-from finance_tracker_shared.schemas import SettingGeneralCreate, SettingDeveloperCreate, SettingViewCreate
+import app.db.models as models
 from app.db.database import get_engine
-# from app.db.utils.seed_db import seed_db_with_data
 from app.core.setup_user_settings import update_all_user_settings_in_file
 from app.core.helpers import read_json_file, load_settings_dict
 
@@ -22,7 +20,7 @@ def setup_database() -> None:
                 raise FileNotFoundError(f"Seed file not found: {APP_CFG['DB_SEED_FILE']}")
             logger.debug(f"Seeding database with file: {APP_CFG['DB_SEED_FILE']}")
             seed_dict = read_json_file(APP_CFG["DB_SEED_FILE"])
-            # seed_db_with_data(seed_dict)
+            seed_db_with_data(seed_dict)
 
         else:
             logger.debug("No seed file provided, seeding settings.")
@@ -41,7 +39,7 @@ def init_db(engine: object=None) -> None:
     if not os.path.exists(APP_CFG["DB_PATH"]):
         new_db = True
         logger.debug(f"Database file does not exist. Creating: {APP_CFG['DB_PATH']}.")
-        Base.metadata.create_all(bind=engine)
+        models.Base.metadata.create_all(bind=engine)
 
     if dispose_after:
         engine.dispose()
@@ -58,10 +56,10 @@ def seed_setting_tables(settings_dict: dict, engine: object=None) -> None:
             if category == "general":
                 for key, value in key_value.items():
                     try:
-                        validated = SettingGeneralCreate(key=key, value=value)
-                        existing = session.query(SettingGeneral).filter_by(key=key).first()
+                        validated = schemas.SettingGeneralCreate(key=key, value=value)
+                        existing = session.query(models.SettingGeneral).filter_by(key=key).first()
                         if not existing:
-                            db_setting = SettingGeneral(**validated.model_dump())
+                            db_setting = models.SettingGeneral(**validated.model_dump())
                             session.add(db_setting)
                             logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
                     except ValidationError as e:
@@ -74,10 +72,10 @@ def seed_setting_tables(settings_dict: dict, engine: object=None) -> None:
                             value = date.today().strftime("%Y-%m-%d")
                             logger.debug(f"Setting start_date to today, ie: {value}")
 
-                        validated = SettingDeveloperCreate(key=key, value=value)
-                        existing = session.query(SettingDeveloper).filter_by(key=key).first()
+                        validated = schemas.SettingDeveloperCreate(key=key, value=value)
+                        existing = session.query(models.SettingDeveloper).filter_by(key=key).first()
                         if not existing:
-                            db_setting = SettingDeveloper(**validated.model_dump())
+                            db_setting = models.SettingDeveloper(**validated.model_dump())
                             session.add(db_setting)
                             logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
                     except ValidationError as e:
@@ -86,10 +84,10 @@ def seed_setting_tables(settings_dict: dict, engine: object=None) -> None:
             elif category == "view":
                 for key, value in key_value.items():
                     try:
-                        validated = SettingViewCreate(key=key, value=value)
-                        existing = session.query(SettingView).filter_by(key=key).first()
+                        validated = schemas.SettingViewCreate(key=key, value=value)
+                        existing = session.query(models.SettingView).filter_by(key=key).first()
                         if not existing:
-                            db_setting = SettingView(**validated.model_dump())
+                            db_setting = models.SettingView(**validated.model_dump())
                             session.add(db_setting)
                             logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
                     except ValidationError as e:
@@ -104,3 +102,93 @@ def re_init_db() -> None:
         logger.info(f"Deleted database file: {APP_CFG['DB_PATH']}")
 
     init_db()
+
+def seed_db_with_data(data_dict: dict, engine: object=None) -> None:
+    def convert_date_strings_in_dict_to_date(data_dict: dict) -> dict:
+        for model, model_data in data_dict.items():
+            if model == "ScheduledTransaction":
+                for entry in model_data:
+                    if "scheduled_date" in entry:
+                        entry["scheduled_date"] = datetime.strptime(
+                            entry["scheduled_date"], "%Y-%m-%d"
+                        ).date()
+            elif model == "TransactionAll":
+                for entry in model_data:
+                    if "date" in entry:
+                        entry["date"] = datetime.strptime(
+                            entry["date"], "%Y-%m-%d"
+                        ).date()
+
+        return data_dict
+    
+    data_dict_converted = convert_date_strings_in_dict_to_date(data_dict)
+    if engine is None:
+        engine = get_engine()
+    logger.debug(f"Seeding DB at {APP_CFG['DB_PATH']}")
+
+    with Session(engine) as session:
+        for model_name, model_data in data_dict_converted.items():
+            model_class = getattr(models, model_name, None)
+            if model_class is None:
+                raise ValueError(f"Model {model_name} not found in app.db.models")
+
+            for item in model_data:
+                try:
+                    if model_name == "AccountAll":
+                        validated = schemas.AccountAllCreate(**item)
+
+                    elif model_name == "AccountCredit":
+                        validated = schemas.AccountCreditCreate(**item)
+                    
+                    elif model_name == "AccountDebit":
+                        validated = schemas.AccountDebitCreate(**item)
+
+                    elif model_name == "AccountIndependent":
+                        validated = schemas.AccountIndependentCreate(**item)
+
+                    elif model_name == "Folio":
+                        validated = schemas.FolioCreate(**item)
+                    
+                    elif model_name == "RecurringTransaction":
+                        validated = schemas.RecurringTransactionCreate(**item)
+
+                    elif model_name == "ScheduledTransaction":
+                        validated = schemas.ScheduledTransactionCreate(**item)
+                    
+                    elif model_name == "SettingDeveloper":
+                        validated = schemas.SettingDeveloperCreate(**item)
+                    
+                    elif model_name == "SettingGeneral":
+                        validated = schemas.SettingGeneralCreate(**item)
+                    
+                    elif model_name == "SettingDeveloper":
+                        validated = schemas.SettingDeveloperCreate(**item)
+
+                    elif model_name == "SettingView":
+                        validated = schemas.SettingViewCreate(**item)
+
+                    elif model_name == "TransactionAll":
+                        validated = schemas.TransactionAllCreate(**item)
+                    
+                    elif model_name == "TransactionExpense":
+                        validated = schemas.TransactionExpenseCreate(**item)
+                    
+                    elif model_name == "TransactionPayment":
+                        validated = schemas.TransactionPaymentCreate(**item)
+
+                    elif model_name == "TransactionTransfer":
+                        validated = schemas.TransactionTransferCreate(**item)
+
+                except ValidationError as e:
+                    raise ValueError(f"Error validating {model_name} with data {item}: {e}")
+
+                try:
+                    existing = session.query(model_class).filter_by(**validated.model_dump()).first()
+                    if not existing:
+                        db_model = model_class(**validated.model_dump())
+                        session.add(db_model)
+                except Exception as e:
+                    raise ValueError(f"Error inserting {model_name} with data {validated}: {e}")
+
+        session.commit()
+        engine.dispose()
