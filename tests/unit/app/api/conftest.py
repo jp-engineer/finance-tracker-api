b4652c08ccf8690989095e102bf3728de5852d2d
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-09 07:42:25 +0100

    REMOVED OLD COMMENTED OUT FIXUTRES

--- tests/unit/app/api/conftest.py
+++ tests/unit/app/api/conftest.py
@@ -13,25 +13,3 @@ def client(tmp_path, monkeypatch):
     monkeypatch.setitem(APP_CFG, "DB_PATH", str(db_path))
     with TestClient(app) as c:
         yield c
-
-# @pytest.fixture()
-# def client_w_test_db(tmp_path, monkeypatch):
-#     db_path = tmp_path / "test_seeded.db"
-#     db_seed_path = "tests/data/app/test_fixture_db_valid_data.json"
-#     monkeypatch.setitem(APP_CFG, "DB_PATH", str(db_path))
-#     monkeypatch.setitem(APP_CFG, "DB_SEED_FILE", db_seed_path)
-
-#     setup_database()
-#     with TestClient(app) as c:
-#         yield c
-
-# @pytest.fixture()
-# def client_w_empty_e2e_db(tmp_path, monkeypatch):
-#     db_path = tmp_path / "test_e2e_blank.db"
-#     monkeypatch.setitem(APP_CFG, "DB_PATH", str(db_path))
-#     monkeypatch.setitem(APP_CFG, "MODE", "e2e_testing")
-
-#     reload_main_module()
-#     from main import app
-#     with TestClient(app) as c:
-#         yield c
