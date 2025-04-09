4d4f798c0ea4f36b04a07f5ccaf0c69488bfadff
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-09 01:05:48 +0100

    added monkeypatching of test db into client fixture

--- tests/unit/app/api/conftest.py
+++ tests/unit/app/api/conftest.py
@@ -1,13 +1,16 @@
 import pytest
 from fastapi.testclient import TestClient
 from main import app
+from app.config import APP_CFG
 
 # from app.config import APP_CFG
 # from app.db.utils.setup_db import setup_database
 # from tests.helpers import reload_main_module
 
 @pytest.fixture()
-def client():
+def client(tmp_path, monkeypatch):
+    db_path = tmp_path / "test.db"
+    monkeypatch.setitem(APP_CFG, "DB_PATH", str(db_path))
     with TestClient(app) as c:
         yield c
 
