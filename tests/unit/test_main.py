601f6410cfa88878c4923edc6dcf38de221e917b
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-10 03:38:21 +0100

    added spaces

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,14 +1,18 @@
 import pytest
 from fastapi import FastAPI
+
 from main import app
+
 
 pytestmark = [
     pytest.mark.unit,
     pytest.mark.api
 ]
 
+
 def test_app_is_fastapi_instance():
     assert isinstance(app, FastAPI)
+
 
 def test_app_title():
     assert app.title == "Finance Tracker API"
