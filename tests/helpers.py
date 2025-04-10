77ddae45948a1afe93a153727fb48829044248df
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-10 23:35:58 +0100

    added load data file helper, and added unit test for checking loaded api routes

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,10 +1,13 @@
+import os
 import sys
 import importlib
+import json
 
 import yaml
 
 
 CONFIG_PATH = "app.config"
+TEST_DATA_DIR = os.path.join("tests", "data")
 
 
 def reload_config_module():
@@ -20,3 +23,21 @@ def create_yaml_file(path, content: dict):
     with open(path, 'w', encoding='utf-8') as f:
         yaml.dump(content, f)
 
+
+def load_test_data_file(file_name: str) -> dict:
+    
+    file_path = os.path.join(TEST_DATA_DIR, file_name)
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"Test data file not found: {file_path}")
+
+    data_dict = {}
+    if file_name.endswith(".json"):
+        with open(file_path, "r", encoding="utf-8") as f:
+            data_dict = json.load(f)
+    elif file_name.endswith(".yml") or file_name.endswith(".yaml"):
+        with open(file_path, "r", encoding="utf-8") as f:
+            data_dict = yaml.safe_load(f)
+    else:
+        raise ValueError("Unsupported file type. Only .json and .yml/.yaml are supported.")
+    
+    return data_dict
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,6 +1,8 @@
 import pytest
 from fastapi import FastAPI
 
+from tests.helpers import load_test_data_file 
+
 from main import app
 
 
@@ -28,3 +30,29 @@ def test_api_prefix(api_prefix):
             assert route.path.startswith(api_prefix)
 
 
+def test_api_routes(api_prefix):
+    loaded_routes = load_test_data_file("api/test_included_api_routes.json")
+    valid_routes = {}
+    for method, list_of_routes in loaded_routes.items():
+        for route in list_of_routes:
+            if method not in valid_routes:
+                valid_routes[method] = [route]
+            else: 
+                valid_routes[method].append(route)
+
+    app_methods = []
+    app_routes = {}
+    for route in app.routes:
+        if route.path not in ["/openapi.json", "/docs", "/docs/oauth2-redirect", "/redoc"]:
+            for method in list(route.methods):
+                if method not in app_methods:
+                    app_methods.append(method)
+    
+    for route in app.routes:
+        if route.path not in ["/openapi.json", "/docs", "/docs/oauth2-redirect", "/redoc"]:
+            for method in list(route.methods):
+                if method not in app_routes:
+                    app_routes[method] = []
+                app_routes[method].append(route.path)
+    
+    assert app_routes == valid_routes
