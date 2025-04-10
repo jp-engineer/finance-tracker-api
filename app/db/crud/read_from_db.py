a86f95aaf0ee3b38b810f1189069a9559b253149
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-10 12:23:25 +0100

    updated styling

--- app/db/crud/read_from_db.py
+++ app/db/crud/read_from_db.py
@@ -1,8 +1,14 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
-from app.db.database import get_engine
-from app.db.models import SettingGeneral, SettingDeveloper, SettingView
+
 from finance_tracker_shared.schemas import SettingGeneralRead, SettingDeveloperRead, SettingViewRead
+
+from app.db.models import SettingGeneral, SettingDeveloper, SettingView
+from app.db.database import get_engine
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 def get_all_settings_from_db_by_category() -> dict:
     engine = get_engine()
@@ -43,14 +49,20 @@ def get_setting_by_category_and_key_from_db(category: str, key: str) -> dict:
             if category == "general":
                 query_result = session.execute(select(SettingGeneral).where(SettingGeneral.key == key)).scalars().first()
                 setting_dict = SettingGeneralRead.model_validate(query_result).model_dump()
+            
             elif category == "developer":
                 query_result = session.execute(select(SettingDeveloper).where(SettingDeveloper.key == key)).scalars().first()
                 setting_dict = SettingDeveloperRead.model_validate(query_result).model_dump()
+            
             elif category == "view":
                 query_result = session.execute(select(SettingView).where(SettingView.key == key)).scalars().first()
                 setting_dict = SettingViewRead.model_validate(query_result).model_dump()
+        
         except Exception as e:
             raise ValueError(f"Error retrieving setting: {e}")
+        
+        logging.debug(f"Retrieved setting: {setting_dict}")
+        
     engine.dispose()
 
     return setting_dict
