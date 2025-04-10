5db7dad585fdfa6f5b707be8b029da0405cf8f3d
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-10 12:20:54 +0100

    re-added has_settings_Tzbles functionality

--- app/core/read_db_cfg.py
+++ app/core/read_db_cfg.py
@@ -1,19 +1,22 @@
 import os
+
 from sqlalchemy import inspect
-# 5from sqlalchemy.orm import Session
-from app.config import APP_CFG
-# from app.db.models.setting import Setting
+from sqlalchemy.orm import Session
+
+from app.db.models.setting_general import SettingGeneral
 from app.db.database import engine_context
+from app.config import APP_CFG
 
 import logging
 logger = logging.getLogger(__name__)
 
-# def check_entries_present_in_settings_table(engine: object) -> bool:
-#     with Session(engine) as session:
-#         count = session.query(Setting).count()
-#         logger.debug(f"settings table entry count: {count}")
 
-#         return count > 0
+def check_entries_present_in_settings_general_table(engine: object) -> bool:
+    with Session(engine) as session:
+        count = session.query(SettingGeneral).count()
+
+        return count > 0
+
 
 def get_db_cfg_dict() -> dict:
     db_path = APP_CFG["DB_PATH"]
@@ -36,16 +39,16 @@ def get_db_cfg_dict() -> dict:
     with engine_context() as engine:
         inspector = inspect(engine)
         table_names = inspector.get_table_names()
-        # has_settings_table = "settings" in table_names
-        # has_data = check_entries_present_in_settings_table(engine) if has_settings_table else False
+        has_settings_table = "settings_general" in table_names
+        has_data = check_entries_present_in_settings_general_table(engine) if has_settings_table else False
 
         cfg = {
             "DB_PATH": db_path,
             "EXISTS": exists,
-            # "HAS_TABLES": has_settings_table,
+            "HAS_TABLES": has_settings_table,
             "TABLES": table_names,
-            "TABLES_COUNT": len(table_names)
-            # "HAS_DATA": has_data
+            "TABLES_COUNT": len(table_names),
+            "HAS_DATA": has_data
         }
 
         logger.info(f"DB config: {cfg}")
