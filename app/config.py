79ee9914ae958361aefb61bc6d106c801cdacaac
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-10 23:06:06 +0100

    updated e2e file routes

--- app/config.py
+++ app/config.py
@@ -66,8 +66,8 @@ DB_SEED_FILE = os.environ.get("DB_SEED_FILE", None)
 SETTINGS_FILE = os.environ.get("SETTINGS_FILE", None)
 
 if MODE == "e2e_testing":
-    DB_PATH = os.path.join("tests", "e2e", "app", "db", DB_FILENAME)
-    SEED_DIR = os.path.join("tests", "e2e", "app", "db", "seed")
+    DB_PATH = os.path.join("tests", "e2e", "db", DB_FILENAME)
+    SEED_DIR = os.path.join("tests", "e2e", "db", "seed")
 
     if DB_SEED_FILE is None:
         DB_SEED_FILE = os.path.join(SEED_DIR, "test_seed.json")
@@ -82,7 +82,7 @@ if MODE == "e2e_testing":
         DB_SEED_FILE = seed_file_str
 
     if SETTINGS_FILE is None:
-        SETTINGS_FILE = os.path.join("tests", "e2e", "app", "user", "test_user_settings.yml")
+        SETTINGS_FILE = os.path.join("tests", "e2e", "user", "test_user_settings.yml")
 
     else:
         settings_file_location = os.path.join("tests", "unit", "app", "user", SETTINGS_FILE)
