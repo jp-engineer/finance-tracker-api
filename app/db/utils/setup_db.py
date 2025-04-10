b207f0c2ec634b346a329e3fe804566e0ab331ea
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-10 04:37:17 +0100

    re-ordered functions and moved check_settings_dict_for_missing_keys to helpers

--- app/db/utils/setup_db.py
+++ app/db/utils/setup_db.py
@@ -9,22 +9,13 @@ import finance_tracker_shared.schemas as schemas
 import app.db.models as models
 from app.db.database import get_engine
 from app.core.setup_user_settings import update_all_user_settings_in_file
-from app.core.helpers import read_json_file, load_user_settings_dict
+from app.core.helpers import read_json_file, load_user_settings_dict, check_settings_dict_for_missing_keys
 from app.db.utils.delete_db import check_for_db_reset
 from app.config import APP_CFG
-from app.core.helpers import SETTINGS_DICT
 
 
 import logging
 logger = logging.getLogger(__name__)
-
-def check_settings_dict_for_missing_keys(input_settings_dict: dict) -> None:
-    for category, setting_list in SETTINGS_DICT.items():
-        for setting in setting_list:
-            if category not in input_settings_dict:
-                raise ValueError(f"Missing category: {category} in settings dictionary.")
-            if setting not in input_settings_dict[category]:
-                raise ValueError(f"Missing setting: {setting} in category: {category}.")        
     
 
 def setup_database() -> None:
@@ -63,67 +54,6 @@ def init_db(engine: object=None) -> None:
         engine.dispose()
 
     return new_db
-
-
-def seed_setting_tables(settings_dict: dict, engine: object=None) -> None:
-    if engine is None:
-        engine = get_engine()
-    logger.debug(f"DB at {APP_CFG['DB_PATH']} with settings: {settings_dict}")
-
-    check_settings_dict_for_missing_keys(settings_dict)
-
-    with Session(engine) as session:
-        for category, key_value in settings_dict.items():
-            if category == "general":
-                for key, value in key_value.items():
-                    try:
-                        validated = schemas.SettingGeneralCreate(key=key, value=value)
-                        existing = session.query(models.SettingGeneral).filter_by(key=key).first()
-                        if not existing:
-                            db_setting = models.SettingGeneral(**validated.model_dump())
-                            session.add(db_setting)
-                            logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
-                    except ValidationError as e:
-                        raise ValueError(f"Error inserting {key} in category {category}: {e}")
-            
-            elif category == "developer":
-                for key, value in key_value.items():
-                    try:
-                        if value is None and key == "start_date":
-                            value = date.today().strftime("%Y-%m-%d")
-                            logger.debug(f"Setting start_date to today, ie: {value}")
-
-                        validated = schemas.SettingDeveloperCreate(key=key, value=value)
-                        existing = session.query(models.SettingDeveloper).filter_by(key=key).first()
-                        if not existing:
-                            db_setting = models.SettingDeveloper(**validated.model_dump())
-                            session.add(db_setting)
-                            logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
-                    except ValidationError as e:
-                        raise ValueError(f"Error inserting {key} in category {category}: {e}")
- 
-            elif category == "view":
-                for key, value in key_value.items():
-                    try:
-                        validated = schemas.SettingViewCreate(key=key, value=value)
-                        existing = session.query(models.SettingView).filter_by(key=key).first()
-                        if not existing:
-                            db_setting = models.SettingView(**validated.model_dump())
-                            session.add(db_setting)
-                            logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
-                    except ValidationError as e:
-                        raise ValueError(f"Error inserting {key} in category {category}: {e}")
-
-        session.commit()
-        engine.dispose()
-
-
-def re_init_db() -> None:
-    if os.path.exists(APP_CFG['DB_PATH']):
-        os.remove(APP_CFG['DB_PATH'])
-        logger.info(f"Deleted database file: {APP_CFG['DB_PATH']}")
-
-    init_db()
 
 
 def seed_db_with_data(data_dict: dict, engine: object=None) -> None:
@@ -243,3 +173,64 @@ def seed_db_with_data(data_dict: dict, engine: object=None) -> None:
         engine.dispose()
 
     update_all_user_settings_in_file(settings_file_dict)
+
+
+def seed_setting_tables(settings_dict: dict, engine: object=None) -> None:
+    if engine is None:
+        engine = get_engine()
+    logger.debug(f"DB at {APP_CFG['DB_PATH']} with settings: {settings_dict}")
+
+    check_settings_dict_for_missing_keys(settings_dict)
+
+    with Session(engine) as session:
+        for category, key_value in settings_dict.items():
+            if category == "general":
+                for key, value in key_value.items():
+                    try:
+                        validated = schemas.SettingGeneralCreate(key=key, value=value)
+                        existing = session.query(models.SettingGeneral).filter_by(key=key).first()
+                        if not existing:
+                            db_setting = models.SettingGeneral(**validated.model_dump())
+                            session.add(db_setting)
+                            logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
+                    except ValidationError as e:
+                        raise ValueError(f"Error inserting {key} in category {category}: {e}")
+            
+            elif category == "developer":
+                for key, value in key_value.items():
+                    try:
+                        if value is None and key == "start_date":
+                            value = date.today().strftime("%Y-%m-%d")
+                            logger.debug(f"Setting start_date to today, ie: {value}")
+
+                        validated = schemas.SettingDeveloperCreate(key=key, value=value)
+                        existing = session.query(models.SettingDeveloper).filter_by(key=key).first()
+                        if not existing:
+                            db_setting = models.SettingDeveloper(**validated.model_dump())
+                            session.add(db_setting)
+                            logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
+                    except ValidationError as e:
+                        raise ValueError(f"Error inserting {key} in category {category}: {e}")
+ 
+            elif category == "view":
+                for key, value in key_value.items():
+                    try:
+                        validated = schemas.SettingViewCreate(key=key, value=value)
+                        existing = session.query(models.SettingView).filter_by(key=key).first()
+                        if not existing:
+                            db_setting = models.SettingView(**validated.model_dump())
+                            session.add(db_setting)
+                            logger.debug(f"Inserted: {key} with value: {value} in category: {category}")
+                    except ValidationError as e:
+                        raise ValueError(f"Error inserting {key} in category {category}: {e}")
+
+        session.commit()
+        engine.dispose()
+
+
+def re_init_db() -> None:
+    if os.path.exists(APP_CFG['DB_PATH']):
+        os.remove(APP_CFG['DB_PATH'])
+        logger.info(f"Deleted database file: {APP_CFG['DB_PATH']}")
+
+    init_db()
