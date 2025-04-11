27e4b0fe08c71e94f66cdf1a2a3fbf6803478006
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-11 14:12:43 +0100

    updated styling

--- app/api/v1/DELETE/delete_e2e_testing.py
+++ app/api/v1/DELETE/delete_e2e_testing.py
@@ -1,12 +1,16 @@
 from fastapi import APIRouter
+
 from finance_tracker_shared.schemas import APIResponse
-from app.db.utils.delete_db import delete_db
+
 from app.core.helpers import check_e2e_mode
+from app.db.utils.delete_db import delete_db
 
 import logging
 logger = logging.getLogger(__name__)
 
+
 router = APIRouter(prefix="/e2e-testing")
+
 
 @router.delete("/db/delete-test-db", response_model=APIResponse)
 def delete_test_db():
