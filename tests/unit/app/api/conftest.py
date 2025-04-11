a8e95f316be6750f36fa1197c19bb011c7fe9144
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-11 04:18:19 +0100

    added e2e client

--- tests/unit/app/api/conftest.py
+++ tests/unit/app/api/conftest.py
@@ -1,7 +1,10 @@
+import os
 import pytest
 from fastapi.testclient import TestClient
 from main import app
+from app.db.utils.setup_db import setup_database
 from app.config import APP_CFG
+from tests.helpers import reload_config_module
 
 # from app.config import APP_CFG
 # from app.db.utils.setup_db import setup_database
@@ -10,6 +13,20 @@ from app.config import APP_CFG
 @pytest.fixture()
 def client(tmp_path, monkeypatch):
     db_path = tmp_path / "test.db"
+    test_seed_file = os.path.join("tests", "unit", "app", "db", "seed", "test_sample.json")
+
+    monkeypatch.setitem(APP_CFG, "DB_SEED_FILE", test_seed_file)
     monkeypatch.setitem(APP_CFG, "DB_PATH", str(db_path))
+
+    setup_database()
+
+    with TestClient(app) as c:
+        yield c
+
+@pytest.fixture()
+def e2e_client():
+    os.environ["MODE"] = "e2e_testing"
+    reload_config_module()
+
     with TestClient(app) as c:
         yield c
