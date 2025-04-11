131bfe2876a9d5a57ab958eb09f49d80eede909c
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-11 01:18:11 +0100

    added seeded db fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,14 @@
+import os
+import shutil
+
 import pytest
+
+from app.db.utils.setup_db import setup_database
+from app.config import APP_CFG
+
+from tests.helpers import reload_config_module
+
+
 
 @pytest.fixture()
 def api_version():
@@ -7,3 +17,15 @@ def api_version():
 @pytest.fixture()
 def api_prefix(api_version):
     return f"/api/{api_version}"
+
+@pytest.fixture()
+def seeded_db(monkeypatch, tmp_path):
+    db_file = tmp_path / "test_finances.db"
+    test_seed_loc = "tests/unit/app/db/seed/test_sample.json"
+
+    monkeypatch.setitem(APP_CFG, "DB_PATH", str(db_file))
+    monkeypatch.setitem(APP_CFG, "DB_SEED_FILE", test_seed_loc)
+
+    setup_database()
+
+    yield db_file
