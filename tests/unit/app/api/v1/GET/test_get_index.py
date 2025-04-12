ba910f7fd592e5e977878a14e61eb3528a3c720f
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-12 10:12:03 +0100

    added newlines

--- tests/unit/app/api/v1/GET/test_get_index.py
+++ tests/unit/app/api/v1/GET/test_get_index.py
@@ -15,12 +15,14 @@ def test_get_index_init_message(client, api_prefix):
     assert response.status_code == 200
     assert response_dict["message"] != None
 
+
 def test_get_index_app_config(client, api_prefix):
     response = client.get(f"{api_prefix}/app/get-config")
     response_dict = response.json()
 
     assert response.status_code == 200
     assert response_dict["data"] != None
+
 
 def test_get_index_db_config(client, api_prefix):
     response = client.get(f"{api_prefix}/db/get-config")
