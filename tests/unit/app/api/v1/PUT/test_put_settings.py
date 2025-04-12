593f4fd9ced47931b7c3b485b064ff36238fb0c6
Jack Pridgeon <jack.pridgeon@outlook.com>
2025-04-12 01:41:31 +0100

    added unhappy test paths

--- tests/unit/app/api/v1/PUT/test_put_settings.py
+++ tests/unit/app/api/v1/PUT/test_put_settings.py
@@ -24,6 +24,14 @@ def test_put_all_settings(client, api_prefix):
     assert response_dict["data"] == True
 
 
+def test_put_all_settings_invalid(client, api_prefix):
+    all_settings_dict = load_test_data_file("api", "test_put_all_settings_invalid.json")
+    response = client.put(f"{api_prefix}/put-all-settings", json=all_settings_dict)
+    response_dict = response.json()
+    
+    assert response.status_code == 422
+
+
 def test_put_general_setting(client, api_prefix):
     general_settings_dict = {
         "key": "default_currency",
@@ -34,6 +42,17 @@ def test_put_general_setting(client, api_prefix):
     
     assert response.status_code == 200
     assert response_dict["data"] != {}
+
+
+def test_put_developer_setting_invalid(client, api_prefix):
+    developer_settings_dict = {
+        "key": "start_date",
+        "value": 1234567890
+        }
+    response = client.put(f"{api_prefix}/developer/put-setting", json=developer_settings_dict)
+    response_dict = response.json()
+    
+    assert response.status_code == 422
 
 
 def test_put_developer_setting(client, api_prefix):
@@ -48,6 +67,17 @@ def test_put_developer_setting(client, api_prefix):
     assert response_dict["data"] != {}
 
 
+def test_put_viewer_setting_invalid(client, api_prefix):
+    viewer_settings_dict = {
+        "key": "user_name",
+        "value": 1234567890
+        }
+    response = client.put(f"{api_prefix}/view/put-setting", json=viewer_settings_dict)
+    response_dict = response.json()
+    
+    assert response.status_code == 422
+
+
 def test_put_viewer_setting(client, api_prefix):
     viewer_settings_dict = {
         "key": "user_name",
@@ -58,3 +88,14 @@ def test_put_viewer_setting(client, api_prefix):
     
     assert response.status_code == 200
     assert response_dict["data"] != {}
+
+
+def test_put_viewer_setting_invalid_key(client, api_prefix):
+    viewer_settings_dict = {
+        "key": "invalid_key",
+        "value": "Testy Mc Test Face"
+        }
+    response = client.put(f"{api_prefix}/view/put-setting", json=viewer_settings_dict)
+    response_dict = response.json()
+    
+    assert response.status_code == 422
